5bc21b9624e746cd7268ad560f7a0f7cfd05e109
Ricardo Henrique <ricardo_henrique007@hotmail.com>
2015-11-10 13:58:00 -0200

    Adicionado os arquivos Sitemap.xml, analytics.js ao projeto e atualizado os arquivo para linkarem o arquivo analytics.min.js

---
+++ assets/js/analytics.js
@@ -0,0 +1,7 @@
+var _gaq = [['_setAccount', 'UA-63633330-4'], ['_trackPageview']];
+(function(d, t){
+	var g = d.createElement(t),
+        s = d.getElementsByTagName(t)[0];
+    g.src = 'http://www.google-analytics.com/ga.js';
+    s.parentNode.insertBefore(g, s);
+}(document, 'script'));
